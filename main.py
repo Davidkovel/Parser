1da83ec8f48234037efc99dad1d65613334d002e
David <113856260+Davidkovel@users.noreply.github.com>
2023-05-25 17:47:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-query = requests.get("https://www.example.com")
-
-if query.status_code == 200:
+try:
+    query = requests.get("https://www.example.com")
     soup = BeautifulSoup(query.content, "html.parser")
     title = soup.find("title").text
     print(title)
-else:
-    print("Немаэ пiдключення ", query.status_code)
+except:
+    print("Немаэ пiдключення")
